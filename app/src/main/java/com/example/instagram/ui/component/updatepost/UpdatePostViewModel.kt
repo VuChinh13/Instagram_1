141ce5a5582f0bc25e290e136cb817181edfecfa
Vũ Văn Chính <vuvanchinh1382k4@gmail.com>
2025-05-12 12:47:09 +0700

    Update UpdatePostViewModel.kt

--- app/src/main/java/com/example/instagram/ui/component/updatepost/UpdatePostViewModel.kt
+++ app/src/main/java/com/example/instagram/ui/component/updatepost/UpdatePostViewModel.kt
@@ -12,12 +12,8 @@ import java.io.File
 
 class UpdatePostViewModel : ViewModel() {
     private val authRepository = AuthRepository()
-
-    // LiveData để lưu trữ kết quả trả về từ Server
     private val _getPostUpdateResponse = MutableLiveData<PostResponse?>()
     val getPostUpdateResponse: LiveData<PostResponse?> = _getPostUpdateResponse
-
-    // khi mà đăng bài thành công thì hiển thị thông báo và tắt Activity đấy đi chuyển sang bên Home
     fun updatePost(
         userId: String,
         postId: String,
